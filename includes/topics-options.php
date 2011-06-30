bc4776b9982eae6409de20eaab7c24bf2e162d20
rockgod100 <jon@ancillaryfactory.com>
2011-06-29 21:01:10 -0500

    Added permission option. Not hooked up yet.

--- includes/topics-options.php
+++ includes/topics-options.php
@@ -8,6 +8,10 @@ function topic_manager_options() {
 
 		$topicsShowInAdminBar = $_POST['topicsShowInAdminBar'];  
         update_option('topicsShowInAdminBar', $topicsShowInAdminBar);
+		
+		$topicManagerPermission = $_POST['topicManagerPermission'];  
+        update_option('topicManagerPermission', $topicManagerPermission);
+		
 
 		
         ?>  
@@ -16,7 +20,8 @@ function topic_manager_options() {
     } else {  
         //Normal page display  
 		
-		$topicsShowInAdminBar = get_option('topicsShowInAdminBar'); 
+		$topicsShowInAdminBar = get_option('topicsShowInAdminBar');
+		$topicManagerPermission = get_option('topicManagerPermission'); 		
     } 
 
 	if (!current_user_can('manage_options'))  {
@@ -26,10 +31,22 @@ function topic_manager_options() {
 		<h2>Settings - Topic Manager</h2>
 		<form name="topics_options_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
 		<input type="hidden" name="topics_updated" value="Y">  
-	
+		
+		<h3>Admin bar</h3>
 		<p><input type="checkbox" name="topicsShowInAdminBar" value="yes" 
 		<?php if ($topicsShowInAdminBar=='yes') { ?> checked <?php } ?> /> Show Topic Manager link in Admin Bar</p>
 		
+		<h3>Permission to use Topic Manager administrator functions</h3>
+	
+		 <label>
+    <input type="radio" name="topicManagerPermission" value="publish_posts" id="status_0"
+	<?php if ($topicManagerPermission=='upload_files') {?> checked <?php } ?> />
+    Authors and Administrators</label>
+  <br>
+  <label>
+    <input type="radio" name="topicManagerPermission" value="manage_options" id="status_1"
+	<?php if ($topicManagerPermission=='manage_options') {?> checked <?php } ?> />
+    Administrators only</label>
 		
 		<p class="submit">  
         <input class="button-primary" type="submit" name="Submit" value="<?php _e('Update Options') ?>" />  
