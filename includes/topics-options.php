84b44309d67779ec2b23397640e7f2029d9b71bf
rockgod100 <jon@ancillaryfactory.com>
2011-07-04 14:51:22 -0500

    Added author mode option to setting page

--- includes/topics-options.php
+++ includes/topics-options.php
@@ -12,7 +12,8 @@ function topic_manager_options() {
 		$topicManagerPermission = $_POST['topicManagerPermission'];  
         update_option('topicManagerPermission', $topicManagerPermission);
 		
-
+		$topicManagerAuthorMode = $_POST['topicManagerAuthorMode'];  
+        update_option('topicManagerAuthorMode', $topicManagerAuthorMode);
 		
         ?>  
         <div class="updated fade"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>  
@@ -21,7 +22,8 @@ function topic_manager_options() {
         //Normal page display  
 		
 		$topicsShowInAdminBar = get_option('topicsShowInAdminBar');
-		$topicManagerPermission = get_option('topicManagerPermission'); 		
+		$topicManagerPermission = get_option('topicManagerPermission');
+		$topicManagerAuthorMode = get_option('topicManagerAuthorMode');  		
     } 
 
 	if (!current_user_can('manage_options'))  {
@@ -32,9 +34,12 @@ function topic_manager_options() {
 		<form name="topics_options_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
 		<input type="hidden" name="topics_updated" value="Y">  
 		
+		
 		<h3>Admin bar</h3>
 		<p><input type="checkbox" name="topicsShowInAdminBar" value="yes" 
 		<?php if ($topicsShowInAdminBar=='yes') { ?> checked <?php } ?> /> Show Topic Manager link in Admin Bar</p>
+		
+		<div style="height:30px">&nbsp;</div>
 		
 		<h3>Permission to use Topic Manager administrator functions</h3>
 	
@@ -47,6 +52,24 @@ function topic_manager_options() {
     <input type="radio" name="topicManagerPermission" value="admin" id="status_1"
 	<?php if ($topicManagerPermission=='admin') {?> checked <?php } ?> />
     Administrators only</label>
+		
+		<div style="height:30px">&nbsp;</div>
+		
+		
+		<h3>Author mode</h3>
+		
+		 <label>
+    <input type="radio" name="topicManagerAuthorMode" value="single" id="mode_0"
+	<?php if ($topicManagerAuthorMode=='single') {?> checked <?php } ?> />
+    Single author mode</label>
+  <br>
+  <label>
+    <input type="radio" name="topicManagerAuthorMode" value="multi" id="mode_1"
+	<?php if ($topicManagerAuthorMode=='multi') {?> checked <?php } ?> />
+    Multi-author mode <em>(adds Author field to each topic and allows admins to email other authors)</em></label>
+	
+	<div style="height:30px">&nbsp;</div>
+	
 		
 		<p class="submit">  
         <input class="button-primary" type="submit" name="Submit" value="<?php _e('Update Options') ?>" />  
